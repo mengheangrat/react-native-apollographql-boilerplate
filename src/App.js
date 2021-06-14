fb67208977f213dfd37b47e757407c75e086bb89
Mengheang Rat <mengheangrat@icloud.com>
2021-06-14 11:25:56 +0700

    config with react-native web

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Text>Hi</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
