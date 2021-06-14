e434f97cac816c90cdf81f3f82e9c3ac4bb468ab
Mengheang Rat <mengheangrat@icloud.com>
2021-06-14 14:42:29 +0700

    Config apollo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {StatusBar, View, StyleSheet} from 'react-native';
+
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 import RootNavigation from './navigations/RootNavigation';
 
+// Initialize Apollo Client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+});
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+});
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <ApolloProvider client={client}>
+      <View style={styles.appContainer}>
+        <StatusBar barStyle="light-content" />
+        <RootNavigation />
+      </View>
+    </ApolloProvider>
+  );
 };
 
 export default App;
